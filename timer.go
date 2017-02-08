2775744279dc6ff92b377466cd53fe8a4b51e2f8
Johnny <johnnibeer@sarbeer.com>
2017-02-08 10:55:24 +0800

    update

--- timer.go
+++ timer.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Now() string {
-	t := time.Date(2017, 3, 1, 0, 0, 0, 0, time.UTC)
+	t := time.Date(2017, 3, 2, 0, 0, 0, 0, time.UTC)
 	if time.Now().Before(t) {
 		return time.Now().UTC().Format("2006-01-02 15:04:05")
 	} else {
